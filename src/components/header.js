20bf4bc74a06f13c012b6a5a5b06a857ebbfa366
nudelx <alex.nudelman@samanage.com>
2017-10-02 11:58:26 +0300

    images

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = ({ children}) => {
+  return (
+    <header className="App-header">
+      {children}
+    </header>
+  )
+
+}
+
+export default Header
---
+++ src/pages/welcome.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+
+const Welcome = () => {
+  return (
+    <div className={'samanageWelcome'}>
+      <div className={'text myBounceDiv'}>{'Dev News'}</div>
+    </div>
+  )
+}
+
+export default Welcome
